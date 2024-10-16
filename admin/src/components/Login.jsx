7887e7ef653b882060544f7c0d2415f7b7391359
NavodMahima <gnmshihara@std.appsc.sab.ac.lk>
2024-10-16 16:56:27 +0530

    Add product Frontend and connect to Backend

--- admin/src/components/Login.jsx
+++ admin/src/components/Login.jsx
@@ -1,19 +1,28 @@
 import React, { useState } from 'react'
+import { backendUrl } from '../App'
+import axios from 'axios'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-const Login = () => {
 
-    const [email,setEmail] = useState('')
-    const [password,setPassword] = useState('')
+const Login = ({setToken}) => {
 
-    const onSubmitHandler = async (e) =>{
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const onSubmitHandler = async (e) => {
         try {
-
             e.preventDefault(); //prevent reload page 
-            
-            
-            
+            const response = await axios.post(backendUrl + '/api/user/admin', { email, password })
+            if (response.data.success) {
+                setToken(response.data.token)
+                toast.success('Login successful!');
+            } else {
+                toast.error(response.data.message)
+            }
         } catch (error) {
-            
+            console.log(error);
+            toast.error(error.message)
         }
     }
     return (
@@ -23,11 +32,11 @@ const Login = () => {
                 <form onSubmit={onSubmitHandler}>
                     <div className='mb-3 min-w-72'>
                         <p className='text-sm font-medium text-gray-700 mb-2'>Email Address</p>
-                        <input onChange={(e)=>setEmail(e.target.value)} value={email} className='rounded-md w-full px-3 py-2 border border-gray-300 outline-none' type="email" placeholder='Your@emai.com' required />
+                        <input onChange={(e) => setEmail(e.target.value)} value={email} className='rounded-md w-full px-3 py-2 border border-gray-300 outline-none' type="email" placeholder='Your@emai.com' required />
                     </div>
                     <div className='mb-3 min-w-72'>
                         <p>Password</p>
-                        <input onChange={(e)=>setPassword(e.target.value)} value={password} className='rounded-md w-full px-3 py-2 border border-gray-300 outline-none' type="password" placeholder='Enter your Password' required />
+                        <input onChange={(e) => setPassword(e.target.value)} value={password} className='rounded-md w-full px-3 py-2 border border-gray-300 outline-none' type="password" placeholder='Enter your Password' required />
                     </div>
                     <button className='mt-2 w-full py-2 px-4 rounded-md text-white bg-black'>Login</button>
                 </form>
--- admin/src/components/NavBar.jsx
+++ admin/src/components/NavBar.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import {assets} from '../assets/assets'
 
-const NavBar = () => {
+
+const NavBar = ({setToken}) => {
+
+  const handleLogout = () => {
+    // Clear token from both localStorage and state
+    localStorage.removeItem('token'); // Step 1: Remove from localStorage
+    setToken(''); // Step 2: Clear from state
+  };
+
   return (
     <div className='flex items-center py-2 px-[4%] justify-between'>
       <img className='w-[max(10%,80px)]' src={assets.logo} alt="" />
-      <button className='bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm'>Logout</button>
+      <button onClick={()=>handleLogout()} className='bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm'>Logout</button>
     </div>
   )
 }
